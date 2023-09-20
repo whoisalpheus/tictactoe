bf44b2d4e1ccda6ef4325dceddca75f07da8402b
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-09-20 11:27:49 -0400

    Add styling, grid to make gameboard

---
+++ main.js
@@ -0,0 +1,28 @@
+// // Module (gameBoard, displayController??)
+// const gameBoard = (() => {
+//     const gameboard = []; // what is this? Do I need this??
+//     // Arrow functions here
+
+//     return {
+//         // My consts
+//     }
+// })();
+
+// gameBoard.const(); // const is what I return in module
+
+// // Factory function for players???
+// const Player = (x, o) => {
+//     const x = () => x;
+//     const o = () => o;
+//     const placeX = () => {
+//         // x turn??
+//     };
+//     const placeO = () => {
+//         // o turn??
+//     }
+//     return {placeX, placeO};
+// };
+
+// const playerOne = Player();
+// const playerTwo = Player();
+// playerOne.placeX();
